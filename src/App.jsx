aff14b6040fd9e09c1c80a8be6af3564ea1dcfac
yan05021 <justyiyan@gmail.com>
2023-03-29 11:22:20 +0200

    React Basics 03 - Pokédex - Afficher des données dans un composant

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Bulbasaur from "./components/PokemonCard";
+import PokemonCard from "./components/PokemonCard";
 
 function App() {
-    const [count, setCount] = useState(0);
+    // const [count, setCount] = useState(0);
 
     return (
         <div>
-            <Bulbasaur />
+            <PokemonCard />
         </div>
     );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,42 @@
 import React from "react";
 
-const Bulbasaur = () => {
-    return (
-        <fiture>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-            <figcaption>bulbasaur</figcaption>
-        </fiture>
-    );
+const card = {
+    width: "200px",
+    padding: "1rem",
+    backgroundColor: "lightgreen",
+    borderRadius: "5px",
+    boxShadow: "0px 0px 5px gray",
+    color: "white",
+    fontSize: "1.6rem",
+    textAlign: "center",
+    textShadow: "0px 0px 5px gray",
 };
-export default Bulbasaur;
+
+const img = {
+    width: "200px",
+};
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
+function PokemonCard() {
+    let pokemon = pokemonList[0];
+    return (
+        <figure style={card}>
+            {pokemon.imgSrc === undefined || null ? (
+                <p>???</p>
+            ) : (
+                <img style={img} src={pokemon.imgSrc}></img>
+            )}
+            <figcaption>bulbasaur</figcaption>
+        </figure>
+    );
+}
+
+export default PokemonCard;
